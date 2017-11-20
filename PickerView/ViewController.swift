2b6fc769a64fe5c0d1cd69385a785d2504a5c686
SoengSaravit <soengsaravit@gmail.com>
2017-11-20 18:03:06 +0700

    Initial Commit

--- PickerView/ViewController.swift
+++ PickerView/ViewController.swift
@@ -10,16 +10,70 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var pickerView: UIPickerView!
+    
+    var date = [Array((1...31).map{ String($0) })  , ["Jan", "Feb","Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"]]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        self.pickerView.dataSource = self
+        self.pickerView.delegate = self
+        
+        let label1 = UILabel()
+        label1.layer.frame.size.width = 55
+        label1.text = "day"
+        
+        let label2 = UILabel()
+        label2.layer.frame.size.width = 55
+        label2.text = "month"
+        
+        self.pickerView.setPickerLabels(labels: [0:label1, 1:label2], containedView: self.pickerView)
     }
+}
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+extension ViewController: UIPickerViewDelegate, UIPickerViewDataSource {
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return date.count
     }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return date[component].count
+    }
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return date[component][row]
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        print(date[component][row])
+    }
+}
 
-
+extension UIPickerView {
+    
+    func setPickerLabels(labels: [Int:UILabel], containedView: UIView) { // [component number:label]
+        
+        let fontSize:CGFloat = 16
+        let labelWidth:CGFloat = containedView.bounds.width / CGFloat(self.numberOfComponents)
+        let x:CGFloat = self.frame.origin.x
+        let y:CGFloat = (self.frame.size.height / 2) - (fontSize / 2)
+        
+        for i in 0...self.numberOfComponents {
+            
+            if let label = labels[i] {
+                
+                if self.subviews.contains(label) {
+                    label.removeFromSuperview()
+                }
+                
+                label.frame = CGRect(x: x + label.frame.width + labelWidth * CGFloat(i), y: y, width: labelWidth, height: fontSize)
+                label.font = UIFont.boldSystemFont(ofSize: fontSize)
+                label.backgroundColor = .clear
+                label.textAlignment = NSTextAlignment.center
+                
+                self.addSubview(label)
+            }
+        }
+    }
 }
 
